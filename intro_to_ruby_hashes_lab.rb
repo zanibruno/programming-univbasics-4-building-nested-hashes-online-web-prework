071d6c0a49b08b52a132ef9806cd353547b36950
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-01 21:56:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 def monopoly_with_fourth_tier
       basic_hash = { 
    :railroads => {:pieces => 4, 
-   :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {:mortgage_value => "$100"}, :pennsylvania_railroad => {:mortgage_value => "$100"}, :b_and_o_railroad => {:mortgage_value => $100}, :shortline_railroad => {:mortgage_value => $100}}}
+   :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {"mortgage_value$" => "$100"}, :pennsylvania_railroad => {:mortgage_value => "$100"}, :b_and_o_railroad => {:mortgage_value => $100}, :shortline_railroad => {:mortgage_value => $100}}}
  }
 
 end
